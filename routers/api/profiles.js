ab250a39e8a40447b1a8b87b5eab079c8ca034c4
Jordan Smith <zdh1207@gmail.com>
2020-02-23 20:12:15 +0800

    profile add, delete and edit function

--- routers/api/profiles.js
+++ routers/api/profiles.js
@@ -5,6 +5,8 @@ const Profiles = require('../../models/Profile');
 
 router.get('/', passportAuthenticate, handlers.getProfiles(Profiles));
 router.get('/:id', passportAuthenticate, handlers.getProfile(Profiles));
-router.post('/add', passportAuthenticate, handlers.addProfile);
+router.post('/add', passportAuthenticate, handlers.addProfile(Profiles));
+router.post('/edit/:id', passportAuthenticate, handlers.editProfile(Profiles));
+router.delete('/delete/:id', passportAuthenticate, handlers.deleteProfile(Profiles));
 
 module.exports = router;

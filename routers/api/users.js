f5c89b76df70b82d539c6f929025782e44808310
Jordan Smith <zdh1207@gmail.com>
2020-02-23 20:31:12 +0800

    client front created

--- routers/api/users.js
+++ routers/api/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const handlers = require("../../lib/handlers");
 const passportAuthenticate = require('../../config/constants').passportAuthenticate;
 
+router.get('/test', (req, res) => {res.send('Test')});
 router.get('/current', passportAuthenticate, handlers.passport);
 router.post('/login', handlers.login);
 router.post('/register', handlers.register);
